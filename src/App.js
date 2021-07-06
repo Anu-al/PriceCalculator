a85990dc4c3c4b8e615e5efea01488fd0c52658a
Anu-al <anu.lingaraju1@gmail.com>
2021-07-06 11:08:35 +0530

    date changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import ExpenseItem from "./components/ExpensItem";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 100,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 2000,
+      date: new Date(2021, 3, 10),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 100000,
+      date: new Date(2021, 7, 8),
+    },
+    {
+      id: "e4",
+      title: "New Desk(wooden)",
+      amount: 5000,
+      date: new Date(2021, 1, 20),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
